c33f873ba4d7b461c76ac4beb1603840901f4d88
mclegarejos <legarejos.mcfernand@gmail.com>
2023-12-08 16:11:19 +0800

    frontend design / responsive

---
+++ index.php
@@ -0,0 +1,274 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coffee and Beyond</title>
+
+
+    <!-- CSS LINK -->
+    <link rel="stylesheet" href="style.css">
+
+    <!-- BOX ICONS -->
+    <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
+
+</head>
+<body>
+    <!--NAVBAR-->
+
+    <header>
+        <a href="#" class="logo">
+            <img src="img/logo.jpg" alt="">
+        </a>
+
+        <!-- MENU ICON -->
+        <i class='bx bx-menu' id="menu-icon"></i>
+
+        <!-- LINKS -->
+        <ul class="navbar">
+            <li><a href="#home">Home</a></li>
+            <li><a href="#about">About Us</a></li>
+            <li><a href="#products">Products</a></li>
+            <li><a href="#customers">Customers</a></li>
+        </ul>
+
+        <!-- ICON -->
+        <div class="header-icon">
+            <i class='bx bx-cart-alt' ></i>
+            <i class='bx bx-search' id="search-icon"></i>
+        </div>
+
+        <!-- SEARCH BOX -->
+        <div class="search-box">
+            <input type="search" name="" id="" placeholder="Search Here...">
+        </div>
+
+
+    </header>
+    <!-- HOME -->
+    <section class="home" id="home">
+        <div class="home-text">
+            <h1>Start your day <br> with coffee </h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Neque repellendus ut labore deleniti odit mollitia doloribus? Asperiores, ratione. Veniam voluptatem sunt accusantium ex numquam? Nesciunt nobis aspernatur dolore nulla excepturi?</p>
+            <a href="#" class="btn">Shop Now</a>
+        </div>
+        <div class="home-img">
+            <img src="img/main.png" alt="">
+        </div>
+    </section>
+
+    <!--ABOUT -->
+    <section class="about" id="about">
+        <div class="about-img">
+            <img src="img/about.jpg" alt="">
+        </div>
+        <div class="about-text">
+            <h2>Our History</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora rerum culpa quos provident unde fugit ex animi, assumenda recusandae corrupti numquam minus beatae, ut exercitationem voluptatibus vel placeat deserunt distinctio!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum officia dignissimos tenetur in possimus?</p>
+            <a href="#" class="btn">Learn More</a>
+        </div>
+    </section>
+
+    <!-- PRODCUCTS -->
+    <section class="products" id="products">
+        <div class="heading">
+            <h2>Our Popular Products</h2>
+        </div>
+    
+        <!-- CONTAINER -->
+        <div class="products-container">
+            <div class="box">
+                <img src="img/p1.jpg" alt="">
+                <h3>Salted Caramel</h3>
+                <div class="content">
+                    <span>₱100</span>
+                    <a href="#">Add to Cart</a>
+                </div>
+            </div>
+
+            <div class="box">
+                <img src="img/p2.jpg" alt="">
+                <h3>Lemon Yakult</h3>
+                <div class="content">
+                    <span>₱100</span>
+                    <a href="#">Add to Cart</a>
+                </div>
+            </div>
+
+            <div class="box">
+                <img src="img/p3.jpg" alt="">
+                <h3>Java Chip Frappe</h3>
+                <div class="content">
+                    <span>₱100</span>
+                    <a href="#">Add to Cart</a>
+                </div>
+            </div>
+            
+            <div class="box">
+                <img src="img/p4.jpg" alt="">
+                <h3>Watermelon Yakult</h3>
+                <div class="content">
+                    <span>₱100</span>
+                    <a href="#">Add to Cart</a>
+                </div>
+            </div>
+
+            <div class="box">
+                <img src="img/p5.jpg" alt="">
+                <h3>Cookies and Cream Frappe</h3>
+                <div class="content">
+                    <span>₱100</span>
+                    <a href="#">Add to Cart</a>
+                </div>
+            </div>
+
+            <div class="box">
+                <img src="img/p6.jpg" alt="">
+                <h3>Mango Graham</h3>
+                <div class="content">
+                    <span>₱100</span>
+                    <a href="#">Add to Cart</a>
+                </div>
+            </div>
+
+            <div class="box">
+                <img src="img/p7.jpg" alt="">
+                <h3>Strawberry Cheesecake</h3>
+                <div class="content">
+                    <span>₱100</span>
+                    <a href="#">Add to Cart</a>
+                </div>
+            </div>
+
+            <div class="box">
+                <img src="img/p8.jpg" alt="">
+                <h3>Hot Cappuccino</h3>
+                <div class="content">
+                    <span>₱100</span>
+                    <a href="#">Add to Cart</a>
+                </div>
+            </div>
+
+        </div>
+    </section>
+
+    <!-- CUSTOMER -->
+    <section class="customers" id="customers">
+        <div class="heading">
+            <h2>Our Customer's</h2>
+        </div>
+
+    <!-- CONTAINER -->
+    <div class="customers-container">
+        <div class="box">
+            <div class="stars">
+                <i class='bx bxs-star' ></i>
+                <i class='bx bxs-star' ></i>
+                <i class='bx bxs-star' ></i>
+                <i class='bx bxs-star' ></i>
+                <i class='bx bxs-star-half'></i>    
+            </div>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore consectetur harum voluptates esse alias dignissimos!</p>
+            <h2>Roger Mae</h2>
+            <img src="img/rev1.jpg" alt="">
+        </div>
+
+        <div class="box">
+            <div class="stars">
+                <i class='bx bxs-star' ></i>
+                <i class='bx bxs-star' ></i>
+                <i class='bx bxs-star' ></i>
+                <i class='bx bxs-star' ></i>
+                <i class='bx bxs-star-half'></i>    
+            </div>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore consectetur harum voluptates esse alias dignissimos!</p>
+            <h2>Mam Maria</h2>
+            <img src="img/rev2.jpg" alt="">
+        </div>
+
+        <div class="box">
+            <div class="stars">
+                <i class='bx bxs-star' ></i>
+                <i class='bx bxs-star' ></i>
+                <i class='bx bxs-star' ></i>
+                <i class='bx bxs-star' ></i>
+                <i class='bx bxs-star-half'></i>    
+            </div>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore consectetur harum voluptates esse alias dignissimos!</p>
+            <h2>Mam Kath</h2>
+            <img src="img/rev3.jpg" alt="">
+        </div>
+    </div>
+    </section>
+    <section class="footer">
+        <div class="footer-box">
+            <h2>Coffee And Beyond</h2>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum, aspernatur.</p>
+            <div class="social">
+                <a href="#"><i class='bx bxl-facebook' ></i></a>
+                <a href="#"><i class='bx bxl-twitter' ></i></a>
+                <a href="#"><i class='bx bxl-instagram' ></i></a>
+                <a href="#"><i class='bx bxl-tiktok' ></i></a>
+            </div>
+        </div>
+        <div class="footer-box">
+            <h3>Support</h3>
+            <li><a href="#">Product</a></li>
+            <li><a href="#">Help & Support</a></li>
+            <li><a href="#">Return Policy</a></li>
+            <li><a href="#">Terms of Use</a></li>
+        </div>
+        <div class="footer-box">
+            <h3>View Guides</h3>
+            <li><a href="#">Features</a></li>
+            <li><a href="#">Careers</a></li>
+            <li><a href="#">Blog Posts</a></li>
+            <li><a href="#">Our Branches</a></li>
+        </div>
+        <div class="footer-box">
+            <h3>Contact</h3>
+            <div class="contact">
+                <span><i class='bx bxs-map'></i>Dasmarinas City, Cavite, Barangay Langkaan 1</span>
+                <span><i class='bx bxs-phone'></i>1 800 8000</span>
+                <span><i class='bx bxs-envelope'></i>coffeeandbeyond@gmail.com</span>
+            </div>
+        </div>
+    </section>
+
+    <!-- COPYRIGHT -->
+    <div class="copyright">
+        <p>Coffee and Beyond & Emsaay All Right Reserved</p>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <!-- JS LINK -->
+    <script src="main.js"></script>
+</body>
+</html>
